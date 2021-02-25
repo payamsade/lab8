5f7b7db8e914ec2d4b2cb7d64db123af9466360e
payamsade <psadeghi@ucsd.edu>
2021-02-25 10:50:50 -0800

    finished up to slide 33

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+L.mapquest.key = 'A68BKbtQGV0LqGKr4ZNJHoSyvJDrABHC';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.878, -117.236],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+L.marker([51.5, -0.09]).addTo(map);
 }
